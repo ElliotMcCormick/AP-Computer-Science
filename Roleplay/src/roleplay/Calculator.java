dd65a31943fca1a392a20a26f2bb25d3ac4b6061
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-08-30 12:56:13 -0600

    Roleplay stuff

---
+++ Roleplay/src/roleplay/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roleplay;
+
+/**
+ *
+ * @author mccormick.elliot19
+ */
+public class Calculator extends CalculatingDevice {
+    
+    @Override
+    public int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+    
+}
