796255b3b3a8eb9a52d68244ac3407738853983b
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-08-30 13:08:04 -0600

    added BlackBoard to roleplay

---
+++ Roleplay/src/roleplay/BlackBoard.java
@@ -0,0 +1,33 @@
+package roleplay;
+
+// A+ Computer Science  -  www.apluscompsci.com
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+
+public class BlackBoard {
+
+    public void paint(Canvas canvas) {
+        smileyFace(canvas);
+    }
+
+    public void smileyFace(Canvas canvas) {
+        GraphicsContext graphics = canvas.getGraphicsContext2D();
+        graphics.setStroke(Color.BLUE);
+        graphics.strokeText("SMILEY FACE LAB ", 35, 35);
+
+        graphics.setFill(Color.YELLOW);
+        graphics.fillOval(210, 100, 400, 400);
+
+        //add more code here
+
+
+    }
+}
