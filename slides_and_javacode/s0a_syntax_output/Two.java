f19e58a904cc0fa0d706e593e1829f78c48f7e88
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-09-04 13:07:33 -0600

    Stars and Stripes lab and basic syntax stuff

--- slides_and_javacode/s0a_syntax_output/Two.java
+++ slides_and_javacode/s0a_syntax_output/Two.java
@@ -10,8 +10,10 @@ public class Two
 {
 	public static void main(String args[])
 	{
-		System.out.println("c\tompsci");
-		System.out.println("com\tpsci");
-		System.out.println("comp\nsci");
+            System.out.println("c\tompsci");
+            System.out.println("com\tpsci");
+            System.out.println("comp\nsci");
+            System.out.println(3 + 3);
+            System.out.println(3 + " " + 3);
 	}	
 }
