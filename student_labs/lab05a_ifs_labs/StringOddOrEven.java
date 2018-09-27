735beb6f92507705f7868665896a3e0e82183d18
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-09-26 20:49:04 -0600

    finished odd or even string lab, now on char analyzer

--- student_labs/lab05a_ifs_labs/StringOddOrEven.java
+++ student_labs/lab05a_ifs_labs/StringOddOrEven.java
@@ -14,20 +14,33 @@ public class StringOddOrEven {
     private String word;
 
     public StringOddOrEven() {
+        setString("");
     }
 
     public StringOddOrEven(String s) {
+        setString(s);
     }
 
     public void setString(String s) {
+        word = s;
     }
 
     public boolean isEven() {
+        if(word.length() % 2 == 0){
+            return true;
+        }
+            
         return false;
     }
 
     public String toString() {
         String output = "";
+        if (isEven()){
+            output += word + " is even.\n";
+        }
+        if (!isEven()){
+            output += word + " is odd.\n";
+        }
         return output;
     }
 }
--- student_labs/lab05a_ifs_labs/StringRunner.java
+++ student_labs/lab05a_ifs_labs/StringRunner.java
@@ -12,5 +12,16 @@ import java.util.Scanner;
 public class StringRunner {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(in);
+        StringOddOrEven test = new StringOddOrEven();
+        
+        for (int i = 0; i < 8; i++){
+            out.print("Enter a word :: ");
+            String word = keyboard.nextLine();
+            test.setString(word);
+            out.println(test.toString());
+            
+        }
+        
     }
 }
