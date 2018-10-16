968595d371cc44dd2dab5fcfaa03350a8c837709
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-10-15 20:35:33 -0600

    lab 09 digit math average thing done

--- student_labs/lab09_while_dowhile/DigitMath.java
+++ student_labs/lab09_while_dowhile/DigitMath.java
@@ -11,16 +11,24 @@ import static java.lang.System.*;
 public class DigitMath {
 
     private static int countDigits(int number) {
-        int sum = 0;
-        return sum;
+        int count = 0;
+        while (number > 0){
+            count++;
+            number /= 10;
+        }
+        return count;
     }
 
     private static int sumDigits(int number) {
         int sum = 0;
+        while(number > 0 ){
+            sum = sum + (number % 10);
+            number /= 10;
+        }
         return sum;
     }
 
     public static double averageDigits(int number) {
-        return 0.0;
+        return (double)sumDigits(number) / countDigits(number);
     }
 }
--- student_labs/lab09_while_dowhile/DigitMathRunner.java
+++ student_labs/lab09_while_dowhile/DigitMathRunner.java
@@ -11,6 +11,20 @@ import static java.lang.System.*;
 public class DigitMathRunner {
 
     public static void main(String args[]) {
+        DigitMath test = new DigitMath();
+        System.out.println("234 has a digit average of " + test.averageDigits(234) + "\n");
+        
+        System.out.println("100000 has a digit average of " + test.averageDigits(10000) + "\n");
+        
+        System.out.println("111 has a digit average of " + test.averageDigits(111) + "\n");
+        
+        System.out.println("9005 has a digit average of " + test.averageDigits(9005) + "\n");
+        
+        System.out.println("84645 has a digit average of " + test.averageDigits(84645) + "\n");
+        
+        System.out.println("8547 has a digit average of " + test.averageDigits(8547) + "\n");
+        
+        System.out.println("123456789 has a digit average of " + test.averageDigits(123456789) + "\n");
         //add test cases 
     }
 }
