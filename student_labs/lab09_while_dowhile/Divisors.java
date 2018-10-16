3dea654336f5c1cf20388391eec6939e0b704906
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-10-16 13:08:03 -0600

    working on divisors lab 09

--- student_labs/lab09_while_dowhile/Divisors.java
+++ student_labs/lab09_while_dowhile/Divisors.java
@@ -9,7 +9,17 @@ package lab09_while_dowhile;
 import static java.lang.System.*;
 
 public class Divisors {
-
+    private int number;
+    
+    public Divisors(){
+        setNumber(0);
+    }
+    public Divisors(int num){
+        setNumber(num);
+    }
+    public void setNumber(int num){
+        number = num;
+    }
     public static String getDivisors(int number) {
         String divisors = "";
         return divisors;
