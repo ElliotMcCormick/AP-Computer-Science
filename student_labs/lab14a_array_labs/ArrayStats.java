35a3ddb6fd9d0b5a4d2ba8ac622d6069718b949d
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-11-12 21:43:46 -0700

    finished all labs for week 11/12, arrays, scanners, etc.

--- student_labs/lab14a_array_labs/ArrayStats.java
+++ student_labs/lab14a_array_labs/ArrayStats.java
@@ -12,14 +12,42 @@ import java.util.Scanner;
 
 public class ArrayStats {
     //instance variable
-
+    private int[] array;
+    
     //constructor
+    public ArrayStats(){
+        this(null);
+    }
+    public ArrayStats(int[] a){
+        setArray(a);
+    }
     //set method
+    public void setArray(int[] a){
+        array = a;
+    }
+    
     public int getNumGroupsOfSize(int size) {
         int cnt = 0;
-
-
-
+        int counter = 0;
+        int startlocation = 0;
+        int currentloc = 0;
+        
+        for (int i = 0; i < array.length; i++){
+            
+            while(!(currentloc > array.length - 1) && array[currentloc] == array[startlocation]){
+                counter++;
+                currentloc++;
+            }
+            
+            if (counter >= size){
+                cnt++;
+                
+            }
+            
+            counter = 0;
+            
+            startlocation = currentloc;
+        }
 
 
         return cnt;
--- student_labs/lab14a_array_labs/ArrayStatsRunner.java
+++ student_labs/lab14a_array_labs/ArrayStatsRunner.java
@@ -9,5 +9,43 @@ package lab14a_array_labs;
 public class ArrayStatsRunner {
 
     public static void main(String args[]) {
+        
+        ArrayStats test = new ArrayStats();
+        
+        int[] a = {3, 3, 3, 3, 3, 9, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8};
+        test.setArray(a);
+        System.out.println(test);
+        System.out.println("size 1 count == " + test.getNumGroupsOfSize(1));
+        System.out.println("size 2 count == " + test.getNumGroupsOfSize(2));
+        System.out.println("size 3 count == " + test.getNumGroupsOfSize(3));
+        System.out.println("size 4 count == " + test.getNumGroupsOfSize(4));
+        System.out.println("size 5 count == " + test.getNumGroupsOfSize(5));
+        System.out.println("size 6 count == " + test.getNumGroupsOfSize(6));
+        
+        System.out.println("");
+        System.out.println("");
+        
+        int[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        test.setArray(b);
+        System.out.println(test);
+        System.out.println("size 1 count == " + test.getNumGroupsOfSize(1));
+        System.out.println("size 2 count == " + test.getNumGroupsOfSize(2));
+        System.out.println("size 3 count == " + test.getNumGroupsOfSize(3));
+        System.out.println("size 4 count == " + test.getNumGroupsOfSize(4));
+        
+        System.out.println("");
+        System.out.println("");
+        
+        int[] c = {1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 4, 4, 4, 6};
+        test.setArray(c);
+        System.out.println(test);
+        System.out.println("size 1 count == " + test.getNumGroupsOfSize(1));
+        System.out.println("size 2 count == " + test.getNumGroupsOfSize(2));
+        System.out.println("size 6 count == " + test.getNumGroupsOfSize(6));
+        System.out.println("size 8 count == " + test.getNumGroupsOfSize(8));
+
+        
+
+        
     }
 }
