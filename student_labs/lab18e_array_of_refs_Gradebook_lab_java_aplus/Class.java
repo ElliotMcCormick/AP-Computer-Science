8813b6c5d9fe7192928f25f51a90a83295e0c4e2
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2018-12-08 13:32:57 -0700

    finished gradebook except for the comparable thing

--- student_labs/lab18e_array_of_refs_Gradebook_lab_java_aplus/Class.java
+++ student_labs/lab18e_array_of_refs_Gradebook_lab_java_aplus/Class.java
@@ -21,47 +21,58 @@ public class Class {
         studentList = new Student[0];
     }
 
-    public Class(String name, int stuCount) {
+    public Class(String nm, int stuCount) {
+        name = nm;
+        studentList = new Student[stuCount];
     }
 
     public void addStudent(int stuNum, Student s) {
+        studentList[stuNum] = s;
     }
 
     public String getClassName() {
-        return "";
+        return name;
     }
 
     public double getClassAverage() {
         double classAverage = 0.0;
-
-
+        for (int i = 0; i < studentList.length; i++){
+            classAverage += studentList[i].getAverage();
+        }
+        
+        classAverage /= studentList.length;
 
         return classAverage;
     }
 
     public double getStudentAverage(int stuNum) {
-        return 0.0;
+        return studentList[stuNum].getAverage();
     }
 
     public double getStudentAverage(String stuName) {
-
-
+        for (int i = 0; i < studentList.length; i++){
+            if (studentList[i].getName().equals(stuName)){
+                return (studentList[i].getAverage());
+            }
+        }
 
         return 0.0;
     }
 
     public String getStudentName(int stuNum) {
-        return "";
+        return studentList[stuNum].getName();
     }
 
     public String getStudentWithHighestAverage() {
         double high = Double.MIN_VALUE;
         String hName = "";
 
-
-
-
-
+        for (int i = 0; i < studentList.length; i++){
+            if (studentList[i].getAverage() > high){
+                hName = studentList[i].getName();
+                high = studentList[i].getAverage();
+            }
+        }
 
         return hName;
     }
@@ -70,9 +81,12 @@ public class Class {
         double low = Double.MAX_VALUE;
         String hName = "";
 
-
-
-
+        for (int i = 0; i < studentList.length; i++){
+            if (studentList[i].getAverage() < low){
+                hName = studentList[i].getName();
+                low = studentList[i].getAverage();
+            }
+        }
 
 
         return hName;
@@ -80,10 +94,12 @@ public class Class {
 
     public String getFailureList(double failingGrade) {
         String output = "";
-
-
-
-
+        
+        for (int i = 0; i < studentList.length; i++){
+            if (studentList[i].getAverage() < failingGrade){
+                output += studentList[i].getName() + " ";
+            }
+        }
 
         return output;
     }
@@ -91,8 +107,11 @@ public class Class {
     public String toString() {
         String output = "" + getClassName() + "\n";
 
-
-
+        for (Student student : studentList){
+            output += student.toString() + " " + student.getAverage() + "\n";
+                    
+        }
+        
 
         return output;
     }
--- student_labs/lab18e_array_of_refs_Gradebook_lab_java_aplus/GradeBookRunner.java
+++ student_labs/lab18e_array_of_refs_Gradebook_lab_java_aplus/GradeBookRunner.java
@@ -14,9 +14,41 @@ import static java.util.Arrays.*;
 public class GradeBookRunner {
 
     public static void main(String args[]) {
-        out.println("Welcome to the Class Stats program!");
+        out.println("Welcome to the Class Stats program!\n");
 
         Scanner keyboard = new Scanner(System.in);
 
+        System.out.print("What is the name of this class? ");
+        String nameOfClass = keyboard.nextLine();
+        
+        
+        System.out.print("How many students are in this class? ");
+        int numberOfStudents =  keyboard.nextInt();
+        keyboard.nextLine();
+        
+        Class theClass = new Class(nameOfClass, numberOfStudents);
+        
+      
+        
+        for (int i = 1; i < numberOfStudents+1; i++){
+            System.out.print("\nEnter the name of student " + i + " : ");
+            String name = keyboard.nextLine();
+            
+            System.out.print("Enter the grades for " + name + "\nUse the format x - grades (2 - 100 100) : ");
+            String gradeString = keyboard.nextLine();
+            
+            Student newStudent = new Student(name, gradeString);
+            
+            theClass.addStudent(i-1, newStudent);
+        }
+        
+        System.out.println(theClass);
+        System.out.println("");
+        
+        out.println("Failure List = " + theClass.getFailureList(70));
+        out.println("Highest Average = " + theClass.getStudentWithHighestAverage());
+        out.println("Lowest Average = " + theClass.getStudentWithLowestAverage());
+
+        out.println("Class Average = " + theClass.getClassAverage());
     }
 }
