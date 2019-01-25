f37f4e499c3cf98401a3879c4efccca757269672
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2019-01-25 13:07:51 -0700

    questions

--- student_labs/elevens/ActivityEightQuestions.java
+++ student_labs/elevens/ActivityEightQuestions.java
@@ -10,5 +10,14 @@ package elevens;
  * @author mccormick.elliot19
  */
 public class ActivityEightQuestions {
+    /*
+    1. Elevens, thirteens, and tens all check to see if matchy.
+    use board and cards and selection and stuff. different size
+    boards and number of cards. 
     
+    2. They are overridden in the implementation of the object
+    this is polymorphism
+    
+    3. They are overidden, yes it does.
+    */
 }
