36ab617002c30ff23fb14f2af3ecb3ab95fb7e43
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2019-01-27 14:07:39 -0700

    added card pics but doesn't work. asked kranz and waiting for answer

---
+++ student_labs/elevens/ActivityNineQuestions.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elevens;
+
+/**
+ *
+ * @author elliot
+ */
+public class ActivityNineQuestions {
+    /*
+    1. size is not a method. it is a variable.
+    2. because that is dealt with in the GUI and doesn't really have to do with 
+        the basic game
+    3. this would work, but it would cause you to have to reuse alot of code for 
+        the different games and would not be as neat. 
+    */
+}
