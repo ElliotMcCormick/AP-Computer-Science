44bc185b7ac79674f5812cdb9c693b16e81aae9e
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2019-02-07 12:46:52 -0700

    working on collide all

---
+++ Breakout/src/breakout/GameState.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package breakout;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.canvas.Canvas;
+
+/**
+ *
+ * @author mccormick.elliot19
+ */
+public class GameState {
+    private List<Renderable> renderables;
+    private List<Updateable> updateables;
+    private List<GameElement> collideables;
+    
+    public GameState(){
+        renderables = new ArrayList();
+        updateables = new ArrayList();
+        collideables = new ArrayList();
+    }
+    
+    public void add(Object object){
+        
+        if (object instanceof Renderable){
+            renderables.add((Renderable)object);
+        }
+        if (object instanceof Updateable){
+            updateables.add((Updateable)object);
+        }
+        if (object instanceof GameElement){
+            collideables.add((GameElement)object);
+        }
+        
+    }
+    
+    public void remove(Object object){
+        if (object instanceof Renderable){
+            renderables.remove((Renderable)object);
+        }
+        if (object instanceof Updateable){
+            updateables.remove((Updateable)object);
+        }
+        if (object instanceof GameElement){
+            collideables.remove((GameElement)object);
+        }
+    }
+   
+    public void updateAll(Canvas canvas){
+        for (Updateable u : updateables){
+            u.update(canvas);
+        }
+    }
+    
+    public void drawAll(Canvas canvas){
+        for (Renderable r : renderables){
+            r.draw(canvas);
+        }
+    }
+    
+    public void collideAll(){
+        for (int element = 0; element < collideables.size() - 1; element++){
+            if (!collideables.get(element).checkCollision(collideables.get(element + 1)).equals(null)){
+                if(collideables.get(element) instanceof Ball){
+                    
+                }
+            }
+            
+        }
+    }
+}
