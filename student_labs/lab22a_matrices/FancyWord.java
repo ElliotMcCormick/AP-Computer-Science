24d6571decb4a7f7c0b66e9c6af91177005ffffe
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2019-02-14 13:08:54 -0700

    just need to get the reverse diagonal working

--- student_labs/lab22a_matrices/FancyWord.java
+++ student_labs/lab22a_matrices/FancyWord.java
@@ -8,19 +8,40 @@ package lab22a_matrices;
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.Arrays;
 
 public class FancyWord {
 
     private String[][] mat;
 
     public FancyWord(String s) {
+        mat = new String[s.length()][s.length()];
         //size the matrix
-        //use Arrays.fill() to fill in the matrix with spaces
+        //use Arrays.fill() to fill in the matrix with spaces\
+        for (int r = 0; r < mat.length; r++){
+            Arrays.fill(mat[r], " ");
+        }
         //use a for loop to load in the letters into the matrix
+        for (int i=0; i<s.length(); i++){
+            mat[0][i] = Character.toString(s.charAt(i));
+            mat[i][i]= Character.toString(s.charAt(i));
+            mat[s.length()-1 - i][s.length()-1 - i] = Character.toString(s.charAt(s.length()-1 - i));
+            mat[s.length() - 1][i] = Character.toString(s.charAt(i));
+        }
+        
+        
+        
+
     }
 
     public String toString() {
         String output = "";
+         for (int r = 0; r < mat.length; r++){
+            for (int c = 0; c < mat[r].length; c++){
+                output += mat[r][c];
+            }
+            output += "\n";
+        }
         return output + "\n\n";
     }
 }
--- student_labs/lab22a_matrices/FancyWordRunner.java
+++ student_labs/lab22a_matrices/FancyWordRunner.java
@@ -17,8 +17,15 @@ public class FancyWordRunner {
         Scanner file = new Scanner(new File("fancyword.dat"));
         int size = file.nextInt();
         file.nextLine();
+        
+        FancyWord fancy;
+        
         for (int i = 0; i < size; i++) {
             String word = file.next();
+            fancy = new FancyWord(word);
+            System.out.println(fancy);
+
         }
+        
     }
 }
