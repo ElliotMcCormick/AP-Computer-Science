8881406de1c90548cf78feef50a93cb4ff8447f2
ElliotMcCormick <mccormick.elliot19@libertycommon.org>
2019-02-14 18:19:06 -0700

    pixLab a5 questions

---
+++ pixLab/Questions/A5_questions.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questions;
+
+/**
+ *
+ * @author elliot
+ */
+public class A5_questions {
+    /*
+    1. no
+    2. yes
+    3. no
+    4. yes
+    5. yes
+    6. yes
+    7. no
+    */
+    
+}
